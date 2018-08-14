0775ac85332b46f2647f14c072ec86555f5beca9
Brent Shaffer <betterbrent@google.com>
2018-08-14 11:40:08 -0700

    WIP

---
+++ appengine/php72/symfony-framework/test/DeployTest.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Cloud\Samples\AppEngine\Symfony;
+
+use Google\Cloud\TestUtils\AppEngineDeploymentTrait;
+use Google\Cloud\TestUtils\ExecuteCommandTrait;
+use Google\Cloud\TestUtils\FileUtil;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class DeployTest extends TestCase
+{
+    use AppEngineDeploymentTrait;
+    use ExecuteCommandTrait;
+
+    public static function beforeDeploy()
+    {
+        // ensure logging output is displayed in phpunit
+        self::$logger = new Logger('phpunit');
+
+        // build the symfony project
+        $tmpDir = sys_get_temp_dir() . '/test-' . FileUtil::randomName(8);
+        self::setWorkingDirectory($tmpDir);
+        self::createSymfonyProject($tmpDir);
+
+        // set the directory in gcloud and move there
+        self::$gcloudWrapper->setDir($tmpDir);
+        chdir($tmpDir);
+    }
+
+    private static function createSymfonyProject($targetDir)
+    {
+        // install
+        $symfonyVersion = 'symfony/symfony-demo:^1.2';
+        $cmd = sprintf('composer create-project %s %s', $symfonyVersion, $targetDir);
+        $process = self::createProcess($cmd);
+        $process->setTimeout(300); // 5 minutes
+        self::executeProcess($process);
+
+        // move the code for the sample to the new symfony installation
+        $files = [
+            'app.yaml',
+            'app/config/config_prod.yml',
+        ];
+        foreach ($files as $file) {
+            $source = sprintf('%s/../%s', __DIR__, $file);
+            $target = sprintf('%s/%s', $targetDir, $file);
+            copy($source, $target);
+        }
+    }
+
+    public function testHomepage()
+    {
+        // Access the blog top page
+        $resp = $this->client->get('/');
+        $this->assertEquals(
+            '200',
+            $resp->getStatusCode(),
+            'top page status code'
+        );
+        $content = $resp->getBody()->getContents();
+        $this->assertContains('Your application is now ready', $content);
+    }
+}
